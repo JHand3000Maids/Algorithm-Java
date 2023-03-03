2d74f885f546b3fe1b3412d0cc7ee6acc42e7965
heejinssss <heejin.dev33@gmail.com>
2023-03-03 15:56:19 +0900

    B_1000.java

--- step1/1000/B_1000.java
+++ step1/1000/B_1000.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.print("Hello World!");
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.print(A+B);
     }
 }
